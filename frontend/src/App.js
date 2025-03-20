63c1f1e0480ebfeb424faf9e3103b27601b3500b
Филипп Мазеин <fhilipmazein@MacBook-Pro-de-Fhilip.local>
2025-03-20 12:42:39 +0100

    Updated files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom'; //BrowserRouter для vps, HashRouter для Git
 import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPage';
 import ContactPage from './pages/ContactPage';
 import PortfolioPage from './pages/PortfolioPage';
 import './components/ServiceCard/ServiceCard.css';
 
+const RouterComponent = process.env.REACT_APP_ENV === 'production' ? BrowserRouter : HashRouter;
 
 function App() {
   return (
-    <Router>
+    <RouterComponent>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/servicios" element={<ServicesPage />} />
         <Route path="/contacto" element={<ContactPage />} />
         <Route path="/resenyas" element={<PortfolioPage />} />
       </Routes>
-    </Router>
+    </RouterComponent>
   );
 }
 

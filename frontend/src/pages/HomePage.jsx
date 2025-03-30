174c92ed1822e842a719b16976750b8f8bf5e830
Филипп Мазеин <fhilipmazein@MacBook-Pro-de-Fhilip.local>
2025-03-30 12:42:37 +0200

    «update»

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,12 +3,10 @@ import Hero from '../components/Hero/Hero';
 
 
 const HomePage = () => {
-  // const [apiMessage, setApiMessage] = useState('');
-
   useEffect(() => {
     fetch('http://localhost:8000/')
       .then(response => response.json())
-      .then(data => setApiMessage(data.message))
+      .then(data => console.log(data.message))
       .catch(error => console.error('Error:', error));
   }, [setApiMessage]);
 
